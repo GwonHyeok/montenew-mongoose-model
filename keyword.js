15463984e299d7b496780f1dd8e5cc3384207a06
GwonHyeok <me@ghyeok.io>
2017-11-24 18:22:32 +0900

    키워드 타입 추가

--- keyword.js
+++ keyword.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const KeyWord = mongoose.model('KeyWord', {
   title: { type: String, required: [true, '키워드 제목이 존재하지 않습니다'] },
   targetUri: { type: String, required: [true, '키워드 타겟 주소가 존재하지 않습니다'] },
-  type: { type: String } // 지식쇼핑, 통합검색 ...
+  type: { type: String, required: [true, '타입이 설정되지 않았습니다'], enum: ['Blog'] }
 });
 
 module.exports = KeyWord;
