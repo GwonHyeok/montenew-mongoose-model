38fd7e2c0656ce6bc2bdfe51b29285e26f3f5c12
GwonHyeok <me@ghyeok.io>
2017-11-24 17:19:43 +0900

    키워드 로그 수정

--- keywordLog.js
+++ keywordLog.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 const KeyWordLog = mongoose.model('KeyWordLog', {
   rank: Number,
-  keyword: { type: mongoose.Schema.Types.ObjectId, ref: 'KeyWord' }
+  keyword: { type: mongoose.Schema.Types.ObjectId, ref: 'KeyWord' },
+  title: String,
+  link: String,
+  description: String,
+  bloggername: String,
+  bloggerlink: String,
+  postdate: String,
 });
 
 module.exports = KeyWordLog;
