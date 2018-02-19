c74df1532032627951cc2a611e0a9bb250ba1377
GwonHyeok <me@ghyeok.io>
2018-02-19 19:16:40 +0900

    키워드 모델 로그 추가

--- keyword.js
+++ keyword.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const KeyWord = mongoose.model('KeyWord', {
   title: { type: String, required: [true, '키워드 제목이 존재하지 않습니다'] },
   targetUri: { type: String, required: [true, '키워드 타겟 주소가 존재하지 않습니다'] },
-  type: { type: String, required: [true, '타입이 설정되지 않았습니다'], enum: ['Blog'] }
+  type: { type: String, required: [true, '타입이 설정되지 않았습니다'], enum: ['Blog'] },
+  logs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'KeyWordLog' }]
 });
 
 module.exports = KeyWord;
