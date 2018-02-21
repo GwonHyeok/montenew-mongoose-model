51b7d9fc65eaaa1a5b06ca312625c6e8bc33ceaa
GwonHyeok <me@ghyeok.io>
2018-02-21 16:26:41 +0900

    보고서 모델 유저 필드 추가

--- report.js
+++ report.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const Report = mongoose.model('Report',
   new Schema({
       title: { type: String, required: true },
+      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
       attachments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Attachment' }]
     },
     {
