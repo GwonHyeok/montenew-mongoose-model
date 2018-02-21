24cf590061fb8fec8e6b4401da5ca9ca2b09eacf
GwonHyeok <me@ghyeok.io>
2018-02-22 00:06:43 +0900

    보고서 항목에 회사 추가

--- report.js
+++ report.js
@@ -5,6 +5,7 @@ const Report = mongoose.model('Report',
   new Schema({
       title: { type: String, required: true },
       author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+      company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
       attachments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Attachment' }]
     },
     {
