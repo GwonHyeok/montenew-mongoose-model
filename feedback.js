f10bf61efcab9376a091bc5fcdf12faae339f7a3
GwonHyeok <me@ghyeok.io>
2018-02-24 18:07:29 +0900

    feedback restify 적용

---
+++ feedback.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Feedback = mongoose.model('Feedback',
+  new Schema({
+      title: { type: String, required: true },
+      content: { type: String, required: true },
+      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+      company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
+      attachments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Attachment' }]
+    },
+    {
+      timestamps: true
+    }
+  )
+);
+
+module.exports = Feedback;
