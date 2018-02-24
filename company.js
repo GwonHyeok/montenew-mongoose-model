89a6fb409db5beb900be9b251e0eaea3c6a29195
GwonHyeok <me@ghyeok.io>
2018-02-25 01:42:17 +0900

    회사 모델 담당자 추가

--- company.js
+++ company.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const Company = mongoose.model('Company',
   new Schema({
       name: { type: String, required: [true, '회사 이름이 존재하지 않습니다'], unique: true },
+      manager: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
       keywords: [{ type: mongoose.Schema.Types.ObjectId, ref: 'KeyWord' }],
       reports: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Report' }],
       medias: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Media' }]
